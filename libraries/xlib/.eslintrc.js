379b2fd4b5911dae741ed1401f2483784bd2bdd0
jasons <jasons@novaleaf.com>
2020-08-08 12:03:41 -0700

    fix mismerge

--- libraries/xlib/.eslintrc.js
+++ libraries/xlib/.eslintrc.js
@@ -4,12 +4,10 @@ require("@rushstack/eslint-config/patch/modern-module-resolution")
 module.exports = {
 	"$schema": "http://json.schemastore.org/eslintrc",
 	//"root": true,  //no impact?
-	//"root": true,
 	"extends": [
 		"@xlib/eslint-config"
 	],
 	"parserOptions": {
 		"tsconfigRootDir": __dirname //required to find the tsconfig.json
-		"tsconfigRootDir": __dirname
 	}
 }
--- utils/eslint-config/.eslintrc.js
+++ utils/eslint-config/.eslintrc.js
@@ -2,65 +2,7 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
 require("@rushstack/eslint-config/patch/modern-module-resolution")
 
-module.exports = {
-	//parserOptions: { },
-	env: {
-		browser: true,
-		es2020: true,
-		node: true,
-		"jest/globals": true,  //https://www.npmjs.com/package/eslint-plugin-jest		
-	},
-	"extends": [
-		"@rushstack/eslint-config",
-		"@rushstack/eslint-config/react",
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		//"plugin:@typescript-eslint/recommended" //dupe
-	],
-	// ! not needed?
-	// "parser": "@typescript-eslint/parser",
-	// "parserOptions": {
-	// 	"ecmaFeatures": {
-	// 		"jsx": true
-	// 	},
-	// 	"ecmaVersion": 11,
-	// 	"sourceType": "module",
-	// 	//tsconfigRootDir: __dirname  //should be set in derived config file!
-	// },
+require("json5/lib/register")
+module.exports = require("./.eslintrc.json5")
 
-	"plugins": [
-		"react",
-		"jest",
-		//"@typescript-eslint" //dupe
-	],
-	overrides: [
-		//! typescript specific
-		{
-			files: ['*.ts', '*.tsx'],
-			"rules": {
-				"indent": [
-					"error",
-					"tab"
-				],
-				"linebreak-style": [
-					"error",
-					"unix"
-				],
-				"quotes": [
-					"error",
-					"double"
-				],
-				"semi": [
-					"error",
-					"never"
-				],
-
-				//https://github.com/typescript-eslint/typescript-eslint/blob/v3.4.0/packages/eslint-plugin/docs/rules/typedef.md
-				"@typescript-eslint/typedef": "off",//use tsconfig strict errors instead		
-			}
-		}
-	],
-
-}
 
