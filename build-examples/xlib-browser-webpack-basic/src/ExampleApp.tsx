6297a38fcfd25fa3b3fcc07a9d3c9629398a4b5c
jasons <jasons@novaleaf.com>
2020-08-25 11:50:42 -0700

    eslint fixes

--- build-examples/xlib-browser-webpack-basic/src/ExampleApp.tsx
+++ build-examples/xlib-browser-webpack-basic/src/ExampleApp.tsx
@@ -14,20 +14,21 @@ export class ExampleApp extends React.Component {
 		}
 
 		return (
-			<div style={{ padding: "20px" }}>
-				<div style={appStyle}>
+			<div style={ { padding: "20px" } }>
+				<div style={ appStyle }>
 					<h2>Hello, world!</h2>
           Here is an example control:
-					<ToggleSwitch leftColor={"#800000"} rightColor={"#008000"} onToggle={this._onToggle} />
+					<ToggleSwitch leftColor={ "#800000" } rightColor={ "#008000" } onToggle={ this._onToggle } />
 				</div>
 			</div>
 		)
 	}
 
-  // React event handlers should be represented as fields instead of methods to ensure the "this" pointer
-  // is bound correctly.  This form does not work with virtual/override inheritance, so use regular methods
-  // everywhere else.
-  private _onToggle = (sender: ToggleSwitch, args: IToggleEventArgs): void => {
-  	console.log("Toggle switch changed: " + args.sliderPosition)
-  };
+
+	// React event handlers should be represented as fields instead of methods to ensure the "this" pointer
+	// is bound correctly.  This form does not work with virtual/override inheritance, so use regular methods
+	// everywhere else.
+	private _onToggle = ( sender: ToggleSwitch, args: IToggleEventArgs ): void => {
+		console.log( "Toggle switch changed: " + args.sliderPosition )
+	};
 }
--- libraries/xlib/src/_main.ts
+++ libraries/xlib/src/_main.ts
@@ -3,8 +3,6 @@ console.log( "Hello, worlds" )
 export const hello = "world rebuild 4"
 
 
-
-
 /** a test func returning hello name */
 export function f( name: string ): string {
 
