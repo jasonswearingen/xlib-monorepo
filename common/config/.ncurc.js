71369df8858a13c60d7e52036cd22ee78d1205f8
jasons <jasons@novaleaf.com>
2020-09-14 11:13:00 -0700

    update rush task to allow project dependency upgrades

---
+++ common/config/.ncurc.js
@@ -0,0 +1,19 @@
+/** config file for npm-check-updates.   https://github.com/raineorshine/npm-check-updates
+ * used by the ```rush dep-check``` task
+ * for additional configuration of that task, see ""./common/config/rush/command-line.json"
+*/
+module.exports = {
+	//"upgrade": false,
+	packageFile: "package.json",
+	/** Remove version ranges from the final package version. 
+	 * best to do this for everything, so we explicitly choose upgrades
+	 */
+	removeRange: true,
+	errorLevel: 2,
+	/** ignore certain packages */
+	"reject": [
+		"@types/node", // works: 14.0.27.  14.10.0 has a bug that doesn't work with webpack.
+	],
+	target: "greatest"
+
+}
